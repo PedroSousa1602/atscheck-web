c58eca7c7f900ce61ae5f309c019bd79f09d7ccf
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-28 14:54:37 +0100

    Update HomePage.jsx to enhance header structure and add logo with buttons

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,14 @@ import './home.css';
 function Home() {
     return (
         <div className="Home">
-            <header className="App-header">
+            <header className="headerHome">
+                <img src="src/pages/images/logo.png" alt="Logo" />
+                <h1><span>Ats</span>Check</h1>
+                <div className="butoes">
+                    <button className="buttonHome">Inicio</button>
+                    <button className="analyzeCV">Analisar CV</button>
+                    <button className="analyzeJob">Analisar CV e Vaga</button>
+                </div>
 
             </header>
         </div>
