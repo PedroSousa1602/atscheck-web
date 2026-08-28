c9b97bd0fe9fe2d106df33a7aed7fcd6fbee96ca
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-28 17:37:24 +0100

    Implement routing for PdfAnalyzer and JobAnalyzer components in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Home from './pages/HomePage';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/HomePage'
+import AnalyzerCV from './pages/PdfAnalyzer'
+import Analyzer from './pages/JobAnalyzer'
+
 
 function App() {
-  return (
-      <div>
-        <Home />
-      </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/PdfAnalyzer" element={<AnalyzerCV />} />
+            <Route path="/JobAnalyzer" element={<Analyzer />} />
+        </Routes>
+    )
 }
 
-export default App;
+export default App
