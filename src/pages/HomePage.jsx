54f0e4454e313af96bb1f9d3cdef027b6e260cfa
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-08-28 15:21:11 +0100

    Wrap logo in a link for improved navigation on HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,10 +4,14 @@ function Home() {
     return (
         <div className="Home">
             <header className="headerHome">
-                <img src="src/pages/images/logo.png" alt="Logo" />
-                <h1><span>Ats</span>Check</h1>
+
+                <a href="/">
+                    <img src="src/pages/images/logo.png" alt="Logo" />
+                </a>
+                <h1>
+                    <span>Ats</span>Check
+                </h1>
                 <div className="butoes">
-                    <button className="buttonHome">Inicio</button>
                     <button className="analyzeCV">Analisar CV</button>
                     <button className="analyzeJob">Analisar CV e Vaga</button>
                 </div>
