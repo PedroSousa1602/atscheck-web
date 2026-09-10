1c16b9a3bd746bac9499d6ee2bf829041666e9d4
Pedro Sousa <145287309+PedroSousa1602@users.noreply.github.com>
2026-09-10 15:53:58 +0100

    Fix import statement for CSS file in PdfAnalyzer component

--- src/pages/PdfAnalyzer.jsx
+++ src/pages/PdfAnalyzer.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from './services/Api';
-import './PdfAnalyze.css';
+import './pdfAnalyze.css';
 
 function PdfAnalyzer() {
     
